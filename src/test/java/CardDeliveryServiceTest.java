e60e650ced069a1936e7e112fcb6a1450a8ec863
Aleksandra <aleksandra.oftin@mail.ru>
2020-02-25 22:48:52 +0300

    Initial commit

---
+++ src/test/java/CardDeliveryServiceTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryServiceTest {
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("chromeoptions.args", "--no-sandbox,--headless,--disable-dev-shm-usage");
+    }
+
+    @Test
+    void shouldSubmitRequest()  {
+        open("http://localhost:9999");
+
+        $$("[type='text']").first().setValue("Казань");
+        $("[type='tel']").setValue("05052020");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $ (withText("Забронировать")).click();
+        $ (withText("Успешно")).waitUntil(visible, 15000);
+        $ (withText("Встреча успешно")).waitUntil(visible, 5000);
+    }
+}
+
